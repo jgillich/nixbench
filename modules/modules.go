52b2bd5f342b59d362dec966d581c4f90a252da5
Jakob Gillich <jakob@gillich.me>
2017-08-16 19:56:46 +0200

    host: disable on openbsd

--- modules/modules.go
+++ modules/modules.go
@@ -1,9 +1,17 @@
 package modules
 
+import "time"
+
 var Modules map[string]Module = map[string]Module{}
 
 type Module interface {
 	Run() error
 	Print()
 	//Name() string
+}
+
+func duration(f func() error) (float64, error) {
+	start := time.Now()
+	err := f()
+	return time.Since(start).Seconds(), err
 }
